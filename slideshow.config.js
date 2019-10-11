4a7901432feda350c6cbf82a80c931f5c7c31366
pjehan <pierre.jehan@gmail.com>
2019-10-11 11:35:08 +0200

    HTML slides

--- slideshow.config.js
+++ slideshow.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   name: 'HTML / CSS',
-  stylesheets: ['assets/css/style.css']
+  stylesheets: ['assets/font-awesome/css/font-awesome.min.css', 'assets/chartjs/Chart.min.css', 'assets/css/style.css'],
+  javascripts: ['assets/chartjs/Chart.bundle.min.js', 'assets/js/script.js']
 }
